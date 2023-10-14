89077d0f0b7ce0ea9ddda0e70e08af93bb2d20cb
harsh-the-innovator <harshgandhi043@gmail.com>
2023-10-14 23:56:34 +0530

    Add two methods for finding and deleting instructor

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,30 +11,49 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
-		return runner -> {
-			createInstructor(appDAO);
-		};
-	}
+    @Bean
+    public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+        return runner -> {
+            //createInstructor(appDAO);
+            //findInstructor(appDAO);
+            deleteInstructor(appDAO);
+        };
+    }
 
-	private void createInstructor(AppDAO appDAO) {
-		Instructor instructor = new Instructor("abc","efg","abc@gmail.com");
-		InstructorDetail instructorDetail = new InstructorDetail("abc.com/youtube","love to code");
+    private void createInstructor(AppDAO appDAO) {
+        Instructor instructor = new Instructor("abc", "efg", "abc@gmail.com");
+        InstructorDetail instructorDetail = new InstructorDetail("abc.com/youtube", "love to code");
 
 		/*Instructor instructor = new Instructor("xyz", "efg", "xyz@gmail.com");
 		InstructorDetail instructorDetail = new InstructorDetail("xyz.com/youtube", "love to code");
 */
-		instructor.setInstructorDetail(instructorDetail);
+        instructor.setInstructorDetail(instructorDetail);
 
-		// Save the instructor
-		// NOTE -  THIS WILL ALSO SAVE THE DETAILS OBJECT BECAUSE OF CascadeType.ALL
-		System.out.println("Saving instructor...\n" + instructor);
-		appDAO.save(instructor);
-		System.out.println("Done!");
-	}
+        // Save the instructor
+        // NOTE -  THIS WILL ALSO SAVE THE DETAILS OBJECT BECAUSE OF CascadeType.ALL
+        System.out.println("Saving instructor...\n" + instructor);
+        appDAO.save(instructor);
+        System.out.println("Done!");
+    }
+
+    private void findInstructor(AppDAO appDAO) {
+        int id = 1;
+        System.out.println("Finding instructor by id: " + id);
+
+        Instructor instructor = appDAO.findById(id);
+
+        System.out.println("Instructor: " + instructor);
+        System.out.println("Detail of the instructor: " + instructor.getInstructorDetail());
+    }
+
+    private void deleteInstructor(AppDAO appDAO) {
+        int id = 1;
+        System.out.println("Deleting instructor with id: " + id);
+        appDAO.deleteById(id);
+        System.out.println("Done!");
+    }
 }
--- src/main/java/com/example/demo/dao/AppDAO.java
+++ src/main/java/com/example/demo/dao/AppDAO.java
@@ -4,4 +4,8 @@ import com.example.demo.entity.Instructor;
 
 public interface AppDAO {
     void save(Instructor instructor);
+
+    Instructor findById(int id);
+
+    void deleteById(int id);
 }
